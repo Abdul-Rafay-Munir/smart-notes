8bfe521e20f790516acb96b78edcc89233c15c70
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-01-28 19:13:07 +0500

    created update note api

--- backend/src/controllers/notes.controller.js
+++ backend/src/controllers/notes.controller.js
@@ -4,7 +4,7 @@ const getNotes = async (req, res) => {
   try {
     const notes = await Notes.find();
 
-    res.status(200).json({ message: "All notes", notes });
+    res.status(200).json({ notes });
   } catch (error) {
     res.status(500).json({ message: "Internal Server error", error });
   }
@@ -16,14 +16,21 @@ const createNote = async (req, res) => {
 
     const note = Notes.create({ title, content });
 
-    res.status(201).json({ message: "Note created", note });
+    res.status(201).json({ message: "Note created", note: note });
   } catch (error) {
     res.status(500).json({ message: "Internal Server error" });
   }
 };
 
-const updateNote = (req, res) => {
-  res.status(200).json({ message: "post updated successfully" });
+const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    await Notes.findByIdAndUpdate(req.params.id, { title, content });
+
+    res.status(200).json({ message: "Note successfully updated" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server error", error });
+  }
 };
 
 const deleteNote = (req, res) => {
