a4957d86905cb18ebffe3fa47c2aadf1ceb8fdbd
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-02-06 19:30:34 +0500

    implemented delete fumctionality

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,22 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utilis";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
 const NoteCard = ({ note }) => {
+  const handlesDelete = async (e, id) => {
+    e.preventDefault();
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+    try {
+      await api.delete(`/notes/${id}`);
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log("Error in handleDelete", error);
+      toast.error("Failed to delete note");
+    }
+  };
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -17,7 +31,12 @@ const NoteCard = ({ note }) => {
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
-            <button className="btn btn-ghost btn-xs text-error">
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => {
+                handlesDelete(e, note._id);
+              }}
+            >
               <Trash2Icon className="size-4" />
             </button>
           </div>
