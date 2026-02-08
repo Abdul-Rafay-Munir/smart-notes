d738bbbe659aff49dbfa1ae6218a4d90d838b18d
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-02-08 20:29:09 +0500

    added navigation

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -38,6 +38,7 @@ const NoteDetailPage = () => {
     try {
       await api.put(`/notes/${id}`, note);
       toast.success("Note updated successfully");
+      navigate("/");
     } catch (error) {
       console.log("Error saving the note:", error);
       toast.error("Failed to update note");
