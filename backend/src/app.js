2d13ddaab83c904e4591c8db03c025b7d7886a74
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-02-19 15:15:32 +0500

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   }),
 );
 app.use(express.json());
