af75269754c604ecec84a106bd7160c22f255be9
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-02-20 10:46:54 +0500

    fix:app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import noteRoutes from "./routes/notes.route.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
 import cors from "cors";
+import userRoute from "./routes/users.route.js";
 
 const app = express();
 
@@ -11,15 +12,17 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
-app.options("/*", cors());
+
 app.use(express.json());
 app.use(rateLimiter);
+
 app.use("/api/notes", noteRoutes);
-
-import userRoute from "./routes/users.route.js";
-
 app.use("/api/auth", userRoute);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 export default app;
