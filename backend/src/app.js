5b7943c6735331ebd0cc15c28bc7bca7bfb1cf11
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-02-20 10:28:40 +0500

    updated order

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,27 +1,21 @@
 import express from "express";
-import cors from "cors";
 import noteRoutes from "./routes/notes.route.js";
-import userRoute from "./routes/users.route.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
+import cors from "cors";
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://smart-notes-ebon.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
-
-app.options("*", cors(corsOptions));
-
+app.use(
+  cors({
+    origin: "",
+  }),
+);
 app.use(express.json());
-
 app.use(rateLimiter);
-
 app.use("/api/notes", noteRoutes);
+
+import userRoute from "./routes/users.route.js";
+
 app.use("/api/auth", userRoute);
 
 export default app;
